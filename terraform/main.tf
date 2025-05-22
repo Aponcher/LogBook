825af24704d9dc18fd0d9f7ac722d2931fe548e2
Adam Poncher <aponcher@mac.com>
2025-05-21 21:07:13 -0500

    fixTFD: tf update

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,17 +136,6 @@ resource "aws_lb_listener" "https" {
   }
 }
 
-resource "aws_lb_listener" "logbook_listener" {
-  load_balancer_arn = aws_lb.logbook_alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.logbook_tg.arn
-  }
-}
-
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.logbook_alb.arn
   port              = 80
