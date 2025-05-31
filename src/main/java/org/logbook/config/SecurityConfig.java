ecd540bc389a1764ece820bb9da3fb55a8b8342a
Adam Poncher <aponcher@mac.com>
2025-05-31 16:08:05 -0500

    exposeHealthCors: small changes to update the requestMatchers

--- src/main/java/org/logbook/config/SecurityConfig.java
+++ src/main/java/org/logbook/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
         http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/login", "/user/register").permitAll()
+                        .requestMatchers("/user/login", "/user/register", "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
